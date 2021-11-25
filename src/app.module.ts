f58c7805934ba533172ce13a1e0c9fc047bed521
Rômulo Machado <romulo.machado@meta.com.br>
2021-11-25 20:08:29 -0300

    Alter env and remove packeges

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
 
 @Module({
   imports: [ConfigModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
